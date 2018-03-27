6b1585bd48514c6afccd8872c1d04ca4f8b773d2
DeathSod <marcelljesus1218@gmail.com>
2018-03-27 04:43:11 -0400

    First commit
    Created repository of project

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>IPN Digital - Home Page</title>
+		<meta charset="utf-8">
+		<!--Bootstrap CSS-->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta http-equiv="x-ua-compatible" content="ie-edge">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<!-- *********** -->
+		<link rel="stylesheet" href="css/stylesheet.css" type="text/css">
+	</head>
+	<body>
+		<div class="bg-init">
+			<header>
+				<nav class="navbar navbar-dark bg-dark justify-content-between">
+					<a class="navbar-brand" href="#">IPN Digital</a>
+					<div>
+						<ul class="login-register">
+							<form class="form-inline" action="#" method="POST" accept-charset="utf-8">
+								<li><input class="" type="text" name="user" placeholder="User or Email"></li>
+								<li><input class="" type="password" name="password" placeholder="Password"></li>
+								<li><button class="btn btn-outline-light" type="submit">Log In</button></li>
+								<li><a href="#"><button class="btn btn-outline-light">Register Now</button></a></li>
+							</form>
+						</ul>
+					</div>
+				</nav>
+			</header>
+			<section class="intro">
+				<div class="intro-text offset-md-1 col-10 text-center"><div>
+					<h1 class="">Buying and selling ads <br/><span>was never easier</span></h1>
+				</div>
+			</section>	
+		</div>
+		<section class="container">
+			<div class="row justify-content-between">
+				<div class="util-ipn">
+					<img class="img-responsive" src="#" alt="">
+					<p><span class="font-weight-bold">Buy</span> ads</p>
+				</div>
+				<div class="util-ipn">
+					<figure class="figure">
+						<img class="figure-img img-fluid rounded" id="item-2" src="./img/sales.png" alt="">
+						<p class="text-center"><span class="font-weight-bold">Sell</span> spaces</span> to DFP</p>
+					</figure>
+				</div>
+				<div class="util-ipn">
+					<figure class="figure">
+						<img class="figure-img img-fluid rounded" src="./img/dfp.png" alt="">
+						<p class="text-center"><span class="font-weight-bold">Connect</span> to DFP</p>
+					</figure>
+				</div>
+				<div class="util-ipn">
+					<figure class="figure">
+						<img class="figure-img img-fluid rounded" src="./img/report.png" alt="">
+						<p class="text-center"><span class="font-weight-bold">Tailored</span> Reports</p>
+					</figure>
+				</div>
+			</div>
+		</section>
+		<footer>
+			
+		</footer>
+		<!-- JQuery * Popper * Bootstrap JS -->
+		<script src="js/jquery-3.2.1.slim.min.js"></script>
+		<script src="js/popper.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<!-- ****************************** -->
+	</body>
+</html>
