3d39b718af36337984b5a27d1bfdc82d50063452
DeathSod <marcelljesus1218@gmail.com>
2018-03-28 08:06:43 -0400

    Finished Home Page
    Added some fonts, icons, changed images, added mid section and footer as
    well as font-awesome.js file.
    The main page for my current screen dimensions is done. Responsive
    design is needed later.

--- index.php
+++ index.php
@@ -12,58 +12,117 @@
 	</head>
 	<body>
 		<div class="bg-init">
-			<header>
-				<nav class="navbar navbar-dark bg-dark justify-content-between">
+			<header class="navbar-fixed-top">
+				<nav class="navbar navbar-dark bg-dark navbar-fixed-top justify-content-between">
 					<a class="navbar-brand" href="#">IPN Digital</a>
-					<div>
-						<ul class="login-register">
+					<div class="form-box">
+						<ul class="login-register row">
 							<form class="form-inline" action="#" method="POST" accept-charset="utf-8">
-								<li><input class="" type="text" name="user" placeholder="User or Email"></li>
-								<li><input class="" type="password" name="password" placeholder="Password"></li>
+								<li><input class="form-control form-control-sm" type="text" name="user" placeholder="User or Email" required="true"></li>
+								<li><input class="form-control form-control-sm" type="password" name="password" placeholder="Password" required="true"></li>
 								<li><button class="btn btn-outline-light" type="submit">Log In</button></li>
-								<li><a href="#"><button class="btn btn-outline-light">Register Now</button></a></li>
 							</form>
+							<li><a href="#"><button class="btn btn-outline-light">Register Now</button></a></li>
 						</ul>
 					</div>
 				</nav>
 			</header>
 			<section class="intro text-center">
-				<h1 class="align-middle">Buying and selling ads <br/><span>was never easier</span></h1>
+				<div style="" class="">
+					<img class="" src="./img/cover-image-3.jpg" alt="">
+				</div>
+				<h1 class="">Buying and selling ads <br/><span>has never been easier</span></h1>
 			</section>	
 		</div>
-		<section class="container">
-			<div class="info row justify-content-between">
-				<div class="util-ipn">
-					<img class="img-responsive" src="#" alt="">
-					<p><span class="font-weight-bold">Buy</span> ads</p>
+		<div class="info-container">
+			<section class="container">
+				<div class="info-desc">
+					<hr class="hr-info">
+					<h2 class="text-center">With us you can</h2>
 				</div>
-				<div class="util-ipn">
-					<figure class="figure">
-						<img class="figure-img img-fluid rounded" src="#" alt="">
-						<p class="text-center"><span class="font-weight-bold">Sell</span> spaces</span></p>
-					</figure>
+				<div class="info row justify-content-between">
+					<div class="util-ipn">
+						<figure class="figure">
+							<img class="figure-img img-fluid rounded" src="./img/buy.svg" alt="">
+							<p class="text-center"><span class="font-weight-bold">Buy</span> ads</span></p>
+						</figure>
+					</div>
+					<div class="util-ipn">
+						<figure class="figure">
+							<img class="figure-img img-fluid rounded" src="./img/sell.svg" alt="">
+							<p class="text-center"><span class="font-weight-bold">Sell</span> spaces</span></p>
+						</figure>
+					</div>
+					<div class="util-ipn">
+						<figure class="figure">
+							<img class="figure-img img-fluid rounded" src="./img/dfp.png" alt="">
+							<p class="text-center"><span class="font-weight-bold">Connect</span> to DFP</p>
+						</figure>
+					</div>
+					<div class="util-ipn">
+						<figure class="figure">
+							<img class="figure-img img-fluid rounded" src="./img/report.svg" alt="">
+							<p class="text-center"><span class="font-weight-bold">Tailored</span> Reports</p>
+						</figure>
+					</div>
 				</div>
-				<div class="util-ipn">
-					<figure class="figure">
-						<img class="figure-img img-fluid rounded" src="./img/dfp.png" alt="">
-						<p class="text-center"><span class="font-weight-bold">Connect</span> to DFP</p>
-					</figure>
-				</div>
-				<div class="util-ipn">
-					<figure class="figure">
-						<img class="figure-img img-fluid rounded" src="#" alt="">
-						<p class="text-center"><span class="font-weight-bold">Tailored</span> Reports</p>
-					</figure>
+			</section>	
+		</div>
+		<footer>
+			<div class="footer-info">
+				<div class="container row text-center justify-content-between">
+					<div class="links-container row">
+						<div class="links-section" id="items-1st">
+							<p>Section 1</p>
+							<hr class="footer-hr">
+							<ul>
+								<a href="#"><li>Link 1</li></a>
+								<a href="#"><li>Link 2</li></a>
+								<a href="#"><li>Link 3</li></a>
+								<a href="#"><li>Link 4</li></a>
+							</ul>
+						</div>
+						<div class="links-section">
+							<p>Section 2</p>
+							<hr class="footer-hr">
+							<ul>
+								<a href="#"><li>Link 1</li></a>
+								<a href="#"><li>Link 2</li></a>
+								<a href="#"><li>Link 3</li></a>
+								<a href="#"><li>Link 4</li></a>
+							</ul>
+						</div>
+						<div class="links-section" id="items-last">
+							<p>Section 3</p>
+							<hr class="footer-hr">
+							<ul>
+								<a href="#"><li>Link 1</li></a>
+								<a href="#"><li>Link 2</li></a>
+								<a href="#"><li>Link 3</li></a>
+								<a href="#"><li>Link 4</li></a>
+							</ul>
+						</div>
+					</div>
+					<div>
+						<div class="sn-container">
+							<h4 class="sn-info">You can find us here</h4>
+							<button class=""><i class="icon fab fa-facebook-f"></i></button>
+							<button><i class="icon fab fa-instagram"></i></button>
+							<button><i class="icon fab fa-twitter"></i></button>
+							<button ><i class="icon fab fa-linkedin-in"></i></button>
+						</div>
+					</div>
 				</div>
 			</div>
-		</section>
-		<footer>
-			
+			<div class="footer-bottom text-center">
+				<p>Here goes the &copy; 2018 - IPN Digital</p>
+			</div>
 		</footer>
 		<!-- JQuery * Popper * Bootstrap JS -->
 		<script src="js/jquery-3.2.1.slim.min.js"></script>
 		<script src="js/popper.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
+		<script src="js/font-awesome.js"></script>
 		<!-- ****************************** -->
 	</body>
 </html>
